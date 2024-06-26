695fe93ea6f617da1468bcd3f688960cec96ed12
Uditha Abesekara <udithaabesekara3@gmail.com>
2024-06-26 09:50:06 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const hamMenu = document.querySelector(".ham-menu");
+const ham = document.querySelector(".ham");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     hamMenu.classList.toggle("active");
+    ham.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav - link").forEach(n => n.
